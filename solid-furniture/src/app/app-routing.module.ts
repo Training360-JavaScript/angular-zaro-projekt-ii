fc4278f56f336101ccc89253f2850544a8eb64c5
Greg Molnar <molnar.gly@gmail.com>
2022-02-08 10:54:36 +0100

    solid-furniture copy done

--- solid-furniture/src/app/app-routing.module.ts
+++ solid-furniture/src/app/app-routing.module.ts
@@ -1,7 +1,43 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EditBillsComponent } from './page/edit-bills/edit-bills.component';
+import { EditCategoriesComponent } from './page/edit-categories/edit-categories.component';
+import { EditCustomersComponent } from './page/edit-customers/edit-customers.component';
+import { EditOrdersComponent } from './page/edit-orders/edit-orders.component';
+import { EditProductsComponent } from './page/edit-products/edit-products.component';
+import { HomeComponent } from './page/home/home.component';
+import { TestComponent } from './page/test/test.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+  },
+  {
+    path: 'edit-categories',
+    component: EditCategoriesComponent,
+  },
+  {
+    path: 'edit-products',
+    component: EditProductsComponent,
+  },
+  {
+    path: 'edit-customers',
+    component: EditCustomersComponent,
+  },
+  {
+    path: 'edit-orders',
+    component: EditOrdersComponent,
+  },
+  {
+    path: 'edit-bills',
+    component: EditBillsComponent,
+  },
+  {
+    path: 'test',
+    component: TestComponent,
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- solid-furniture/src/app/app.module.ts
+++ solid-furniture/src/app/app.module.ts
@@ -3,10 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SidebarComponent } from './common/sidebar/sidebar.component';
+import { HomeComponent } from './page/home/home.component';
+import { HeaderComponent } from './common/header/header.component';
+import { EditCategoriesComponent } from './page/edit-categories/edit-categories.component';
+import { EditProductsComponent } from './page/edit-products/edit-products.component';
+import { EditCustomersComponent } from './page/edit-customers/edit-customers.component';
+import { EditOrdersComponent } from './page/edit-orders/edit-orders.component';
+import { EditBillsComponent } from './page/edit-bills/edit-bills.component';
+import { TestComponent } from './page/test/test.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    HomeComponent,
+    HeaderComponent,
+    EditCategoriesComponent,
+    EditProductsComponent,
+    EditCustomersComponent,
+    EditOrdersComponent,
+    EditBillsComponent,
+    TestComponent
   ],
   imports: [
     BrowserModule,
