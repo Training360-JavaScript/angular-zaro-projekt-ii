c5c5252d3583ee9c1eaa1799dc2c0eb354838185
Greg Molnar <molnar.gly@gmail.com>
2022-02-14 18:14:58 +0100

    Comment on how to open Edits added to products component

--- solid-furniture/src/app/page/products/products.component.ts
+++ solid-furniture/src/app/page/products/products.component.ts
@@ -1,3 +1,4 @@
+import { Router } from '@angular/router';
 import { ProductService } from './../../service/product.service';
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
@@ -6,24 +7,37 @@ import { Product } from 'src/app/model/product';
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
-  styleUrls: ['./products.component.scss']
+  styleUrls: ['./products.component.scss'],
 })
 export class ProductsComponent implements OnInit {
-
   constructor(
-    private pService: ProductService
-  ) { }
+    private pService: ProductService,
+    private router: Router //GERGO ADDED
+  ) {}
 
   allProducts$: Observable<Product[]> = this.pService.getAll();
-  searchCategory: string = ''
-  keyword: string = ''
+  searchCategory: string = '';
+  keyword: string = '';
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  //How ID nav works GERGO ADDED
+  /*
+  addProduct() {
+    this.router.navigate(['/edit-products', 0]);
   }
+
+  deleteProduct(id: number) {
+    this.pService.delete(id).subscribe(() => {
+      this.allProducts$ = this.pService.getAll();
+    });
+  }
+  */
 
   onDelete(product: Product): void {
-    if (!confirm('Are you sure?')) { return } //confirm ablakkal könnyen megvalósítható a biztonsági kérdés
-    this.pService.delete(product.id)
+    if (!confirm('Are you sure?')) {
+      return;
+    } //confirm ablakkal könnyen megvalósítható a biztonsági kérdés
+    this.pService.delete(product.id);
   }
-
 }
