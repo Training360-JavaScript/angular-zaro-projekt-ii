a5b4d1e132003ba190319a50241dad0b2740d769
Gábor Vass <90134776+Vassgabi89@users.noreply.github.com>
2022-02-15 10:37:40 +0100

    try to merge - product page komment

--- solid-furniture/src/app/page/products/products.component.ts
+++ solid-furniture/src/app/page/products/products.component.ts
@@ -1,3 +1,52 @@
+import { Router } from '@angular/router';
+import { ProductService } from './../../service/product.service';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Product } from 'src/app/model/product';
+
+@Component({
+  selector: 'app-products',
+  templateUrl: './products.component.html',
+  styleUrls: ['./products.component.scss'],
+})
+export class ProductsComponent implements OnInit {
+  constructor(
+    private pService: ProductService,
+    private router: Router //GERGO ADDED
+  ) {}
+
+  allProducts$: Observable<Product[]> = this.pService.getAll();
+  searchCategory: string = '';
+  keyword: string = '';
+
+  ngOnInit(): void {}
+
+  //How ID nav works GERGO ADDED
+  /*
+  addProduct() {
+    this.router.navigate(['/edit-products', 0]);
+  }
+  CORRESPONDING HTML SNIPPET
+  (click)="createProduct()
+  deleteProduct(id: number) {
+    this.pService.delete(id).subscribe(() => {
+      this.allProducts$ = this.pService.getAll();
+    });
+  }
+  CORRESPONDING HTML SNIPPET
+   <button (click)="deleteProduct(product.id)"
+   Edit button is the same
+  */
+
+  onDelete(product: Product): void {
+    if (!confirm('Are you sure?')) {
+      return;
+    } //confirm ablakkal könnyen megvalósítható a biztonsági kérdés
+    this.pService.delete(product.id);
+  }
+}
+
+/*
 import { ProductService } from './../../service/product.service';
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
@@ -26,3 +75,4 @@ export class ProductsComponent implements OnInit {
 
   ngOnInit(): void {}
 }
+*/
