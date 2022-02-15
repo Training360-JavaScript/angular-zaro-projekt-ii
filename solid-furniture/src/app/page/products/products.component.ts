a5562f9d78fe18f74325e7cdc438b1f97844a1fa
Gábor Vass <90134776+Vassgabi89@users.noreply.github.com>
2022-02-15 10:58:57 +0100

    szűrő mező bővítés, sticky fejlécek

--- solid-furniture/src/app/page/products/products.component.ts
+++ solid-furniture/src/app/page/products/products.component.ts
@@ -1,7 +1,7 @@
 import { ProductService } from './../../service/product.service';
+import { Product } from 'src/app/model/product';
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
-import { Product } from 'src/app/model/product';
 
 @Component({
   selector: 'app-products',
@@ -12,13 +12,35 @@ export class ProductsComponent implements OnInit {
 
   constructor(
     private pService: ProductService
-  ) { }
+  ) {
+  }
 
-  allProducts$: Observable<Product[]> = this.pService.getAll();
-  searchCategory: string = ''
+  allProducts$: Observable<Product[]> = this.pService.getAll()
+  productKeys: string[] = Object.keys(new Product())
+
+  searchKey: string = 'name'
   keyword: string = ''
+  keywordMin: string = ''
+  keywordMax: string = ''
 
   ngOnInit(): void {
+
+  }
+
+  console():void {
+    console.log(this.searchKey)
+    console.log(this.keywordMin)
+    console.log(this.keywordMax)
+  }
+
+  clearKeyword():void {
+    this.keyword = ''
+    this.console()
+  }
+
+  clearKeywordMinMax():void {
+    this.keywordMin = ''
+    this.keywordMax = ''
   }
 
   onDelete(product: Product): void {
--- solid-furniture/src/app/pipe/filter.pipe.ts
+++ solid-furniture/src/app/pipe/filter.pipe.ts
@@ -5,23 +5,47 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class FilterPipe implements PipeTransform {
 
-  // transform(value: unknown, ...args: unknown[]): unknown {
-  //   return null;
-  // }
-
-  transform(value: any[] | null, phrase: string = '', key: string = ''): any[] | null {
-    if (!Array.isArray(value) || !phrase) {
+  transform(value: any[] | null, phrase: string = '', phraseMin: string = '',phraseMax: string = '',key: string = ''): any[] | null {
+    if (!Array.isArray(value) || !phrase && (!phraseMin && !phraseMax)) {
       return value;
     }
 
-    phrase = phrase.toLowerCase();
-
+    //search in every category
     if (!key) {
       return value.filter(
         item => Object.values(item).join('').toLowerCase().includes(phrase)
       );
     }
 
+    //search in boolean types
+    if (typeof phrase === 'boolean') {
+      return value.filter(item =>
+        item[key] === phrase
+      )
+    }
+
+    //search in number types in an intervall
+    if ((phraseMin || phraseMax) || ((phraseMin && phraseMax) && (phraseMin < phraseMax))) {
+      console.log('na nézzük')
+      if (phraseMin && !phraseMax)
+      {
+        return value.filter(item =>
+          item[key] >= phraseMin)
+      }
+      if (!phraseMin && phraseMax)
+      {
+        return value.filter(item =>
+          item[key] <= phraseMax)
+      }
+      if (phraseMin && phraseMax)
+      {
+        return value.filter(item =>
+          item[key] >= phraseMin && item[key] <= phraseMax)
+      }
+    }
+
+    //search in string types
+    phrase = phrase.toLowerCase();
     return value.filter(item => {
       const data = String(item[key]).toLowerCase();
       return data.includes(String(phrase));
